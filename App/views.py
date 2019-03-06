4da29f4ccd8e6eed6e4ae48ad51177a8f4904961
bohnsix <bohn_six@163.com>
2019-03-06 09:50:15 +0800

    modified

--- App/views.py
+++ App/views.py
@@ -2,15 +2,15 @@ import random
 
 from flask import Blueprint, render_template
 
-blue = Blueprint('first_blue', __name__)
+app_blue = Blueprint('app', __name__)
 
 
-@blue.route('/')
+@app_blue.route('/')
 def index():
     return render_template('index.html')
 
 
-@blue.route('/video_page/<title>/')
+@app_blue.route('/video_page/<title>/')
 def video_page(title):
     return render_template("video_page.html", title=title)
 
--- manager.py
+++ manager.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
 
-from App.views import blue
+from App.views import app_blue
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234567890'
 
-app.register_blueprint(blueprint=blue)
+app.register_blueprint(blueprint=app_blue)
 
 manager = Manager(app)
 Bootstrap(app)
